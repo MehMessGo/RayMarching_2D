2f323f7332e73483454832999535e80936543c1c
Michael Pakhmurin <pmdmix1@gmail.com>
2021-09-28 15:46:38 +0700

    Созданы функции отрисовки круга и квадрата
    Отрисовка круга/квадрата и обводка фигуры

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+function drawRectangle(x, y, width, height, color = 'black', borderWidth = 0, borderColor = 'transparent'){
+    context.fillStyle = color;
+    context.fillRect(x, y, width, height);
+    context.lineWidth = borderWidth;
+    context.strokeStyle = borderColor;
+    context.strokeRect(x, y, width, height);
+}
 
-context.fillStyle='#ecdab9';
-context.fillRect(0,0,canvas.width,canvas.height);
+function drawCircle(x, y, radius, color = 'black', borderWidth = 0, borderColor = 'transparent'){
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = borderWidth;
+    context.strokeStyle = borderColor;
+    context.stroke();
+}
+
+let context = canvas.getContext('2d');
+
+drawRectangle(
+    x = 0, y = 0, 
+    width = context.canvas.width, height = context.canvas.height,
+    color = '#ecdab9'
+);
+
+drawCircle(70, 70, 10, 'yellow');
